2d4813896d896db3732ef960d59611fc65eb9c3f
Tony Li <crazygemini.lee@gmail.com>
2011-10-06 12:33:41 +0800

    Shorten url using goo.gl

--- shorten.js
+++ shorten.js
@@ -2,19 +2,28 @@ var shortener = {
   "providers": [
     { "index": 0, "url":"http://goo.gl", "name":"google"}
   ],
-  "shorten": function(index, url) {
-    if (arguments.length == 2) {
+  "shorten": function(index, url, callback) {
+    if (arguments.length == 3) {
       if ("string" == typeof index) {
         index = parseInt(index);
       }
       var func = shortener[ shortener.providers[ index ].name ];
       if (func) {
-        return func(url);
+        func(url, callback);
       }
     }
   },
-  "google": function(url) {
-    return "http://www.google.com";
+  "google": function(url, callback) {
+    $.ajax({
+        "url": "https://www.googleapis.com/urlshortener/v1/url",
+        "contentType": "application/json",
+        "type": "POST",
+        "data": "{\"longUrl\": \"" + url + "\"}",
+        "dataType": "json",
+        "success": function(data) {
+          callback(data[ "id" ], data[ "longUrl" ]);
+        }
+        });
   }
 };
 
